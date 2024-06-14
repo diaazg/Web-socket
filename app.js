be6e7d948e7cd809e6193cd154dd0e7b4b7e1819
diaazg <mohamedzellagui585@gmail.com>
2024-06-14 04:21:21 +0100

    post request

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const express = require('express');
-const pool = require(`./client`)
+const {pool} = require(`./client`)
 
 const app = express();
 const port = 3000;
 
+app.use(express.json()); 
 
-pool.on('connect', () => {
-    console.log('Connected to the database');
-  });
+
+
+
+  app.post('/users', async (req, res) => {
+    console.log(req.body)
+    const { username,email, password  } = req.body;
   
-  pool.on('error', (err) => {
-    console.error('Unexpected error on idle client', err);
-    process.exit(-1);
+    if (!username || !password || !email) {
+      return res.status(400).json({ error: 'Username and password are required' });
+    }
+  
+    try {
+      const result = await pool.query(
+        'INSERT INTO users (username, password,email) VALUES ($1, $2,$3) RETURNING *',
+        [username, password,email]
+      );
+      res.status(201).json(result.rows[0]);
+    } catch (error) {
+      console.error('Error inserting data:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   });
 
-  app.use('/',()=>{
-    console.log('fffffffffffffff')
-  })
+
 
   app.listen(port,()=>{
     console.log(`server is running on port : ${port}`)
--- client.js
+++ client.js
@@ -9,5 +9,15 @@ const pool = new Pool({
     port: 5432, 
   });
 
-  module.exports = pool
+  pool.on('connect', () => {
+    console.log('Connected to the database');
+  });
+  
+  pool.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+  });
+
+
+  module.exports = {pool}
 
